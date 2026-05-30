840e2f5befea5a85374b7536c2c4fd6364b9f8b4
Mehri <mehribannusretova2020@gmail.com>
2026-05-30 12:54:37 +0400

    Initialize MasterFix project structure

---
+++ src/main/java/com/example/masterfix/MasterFixApplication.java
@@ -0,0 +1,13 @@
+package com.example.masterfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterFixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterFixApplication.class, args);
+    }
+
+}
