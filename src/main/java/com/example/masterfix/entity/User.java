7e5a4e346959371947d062f1f344555c6b3cc2db
Mehri <mehribannusretova2020@gmail.com>
2026-06-03 16:36:42 +0400

    Add user entity with security integration

---
+++ src/main/java/com/example/masterfix/entity/User.java
@@ -0,0 +1,105 @@
+package com.example.masterfix.entity;
+
+import com.example.masterfix.enums.Role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User implements UserDetails {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 16)
+    String firstName;
+
+    @NotBlank
+    @Size(min = 3, max = 16)
+    String lastName;
+
+    @NotBlank
+    @Column(nullable = false,unique = true)
+    @Size(min = 3, max = 16)
+    String userName;
+
+    @NotBlank
+    @Email
+    @Column(nullable = false, unique = true)
+    String email;
+
+    @NotBlank()
+    @Pattern(regexp = "^[a-zA-Z0-9]{6,16}$", message = "Username must be 6-12 alphanumeric characters")
+    @Column(nullable = false)
+    String password;
+
+    @NotBlank(message = "Telefon nömrəsi boş ola bilməz")
+    @Column(nullable = false, unique = true)
+    @Pattern(regexp = "^(\\+994|0)(10|5[015]|7[07]|99)\\d{7}$", message = "Invalid Azerbaijan phone number format")
+    String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    Role role;
+
+    @Column(nullable = false)
+    private boolean active = true;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+       return List.of(
+                new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
+}
+
---
+++ src/main/java/com/example/masterfix/enums/Role.java
@@ -0,0 +1,7 @@
+package com.example.masterfix.enums;
+
+public enum Role {
+    User,
+    Admin,
+    Master
+}
