312a685ad4a2db46b63b8e99ff48f92d0134032c
Mehri <mehribannusretova2020@gmail.com>
2026-06-05 19:42:17 +0400

    Add JWT configuration properties

--- src/main/java/com/example/masterfix/config/JwtConfig.java
+++ src/main/java/com/example/masterfix/config/JwtConfig.java
@@ -1,4 +1,17 @@
 package com.example.masterfix.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "jwt")
 public class JwtConfig {
+
+    private String secretKey;
+
+    private Long expiration;
 }
