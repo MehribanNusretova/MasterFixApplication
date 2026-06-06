af62e6c0eb6a2ead8a84c7609b91f922c82b4c76
Mehri <mehribannusretova2020@gmail.com>
2026-06-06 18:23:34 +0400

    Add custom user details service

---
+++ src/main/java/com/example/masterfix/security/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.example.masterfix.security;
+
+import com.example.masterfix.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User tapılmadı: " + email));
+
+    }
+}
